c827de6b692848356d67367bc226ccc00fe26556
liumin1128 <970568830@qq.com>
2017-07-17 00:20:42 +0800

    更新脚本

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
 #!/usr/bin/env node
 
-const fs = require('fs-extra')
+var fs = require('fs-extra')
 var path = require('path');
-
-
-function copy (from, to) {
-  from = path.join(__dirname, 'templates', from);
-  write(to, fs.readFileSync(from, 'utf-8'))
-}
-function write (path, str, mode) {
-  fs.writeFileSync(path, str)
-}
-function mkdir (path, fn) {
-  fs.mkdir(path, function (err) {
-    fn && fn()
-  })
-}
-
-console.log(process.argv);
 
 var commend = process.argv[2]
 var dirName = process.argv[3]
 
-
 var templates = path.join(__dirname, 'templates')
-
-console.log(templates)
 
 switch (commend) {
     case 'new':
         try {
             fs.copySync(templates, dirName)
             console.log('初始化完成!')
+            console.log('编译动态链接库：npm run dll')
+            console.log('开启编辑模式：  npm start')
+            console.log('打包项目：     npm run build')
         } catch (err) {
             console.error(err)
         }
