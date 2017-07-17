a56be4b1082cab9023b8db547bd2bce80f5e94b2
liumin1128 <970568830@qq.com>
2017-07-17 09:22:25 +0800

    🏀

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs-extra')
 var path = require('path');
 
 var commend = process.argv[2]
-var dirName = process.argv[3]
+var dirName = process.argv[3] || './'
 
 var templates = path.join(__dirname, 'templates')
 
@@ -13,9 +13,10 @@ switch (commend) {
         try {
             fs.copySync(templates, dirName)
             console.log('初始化完成!')
+            console.log('安装依赖：       cnpm i')
             console.log('编译动态链接库：npm run dll')
             console.log('开启编辑模式：  npm start')
-            console.log('打包项目：     npm run build')
+            console.log('打包项目：       npm run build')
         } catch (err) {
             console.error(err)
         }
